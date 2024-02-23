f7514871940b8c7f4985dcfe47afec0ca054f12d
lakshag32 <lakshag32@gmail.com>
2024-02-23 11:23:01 -0800

    move objectsuppler.get() in

--- src/main/java/frc/robot/commands/vision/DriveToNote.java
+++ src/main/java/frc/robot/commands/vision/DriveToNote.java
@@ -40,7 +40,6 @@ public class DriveToNote extends Command {
    */
   @Override
   public void initialize(){
-    object = objectSupplier.get();
     drive.stop();
     start_time = System.currentTimeMillis(); 
     execute_run_counter = 0;
@@ -70,12 +69,13 @@ public class DriveToNote extends Command {
 
       //Get a reading of the x-offset. Only do this once i.e the first time we are over 40 ms i.e when execute_run_counter is 1. 
       if(execute_run_counter == 1){
-        ;//angle = object.getCameraRelativeAngle(); 
+        object = objectSupplier.get();
+        angle = object.getRelativeAngle(); 
       }
       
       //All other times that we are over 40 ms, just drive with the angle towards the note.  
       if(execute_run_counter >1){
-        ;//drive.driveHeading(speed*Math.cos(angle), speed*Math.sin(angle), angle, true);
+        drive.driveHeading(speed*Math.cos(angle), speed*Math.sin(angle), angle, true);
       }
     }
 
